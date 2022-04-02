2a9a5e1da3cfb359acc27eabc2ebf86459b45d02
Christopher Langner <langnerc@icloud.com>
2022-04-02 09:40:42 -0500

    added logic for no high score to diplay a message on the highscore page

--- assets/highScore.js
+++ assets/highScore.js
@@ -3,14 +3,17 @@ const score = document.getElementById("score");
 const scoreContainer = document.getElementById("score-container");
 let getScore = localStorage.getItem("high score");
 let getInitials = localStorage.getItem("initials");
+if (!getScore) {
+  const blankScore = document.createElement("h4");
+  blankScore.innerText = `There are no high scores yet, play the game to see the score displayed here.`;
+  score.appendChild(blankScore)
+} else {
+  //map scores to UI
+  const scoreDisplay = document.createElement("h3");
+  scoreDisplay.innerHTML = `High score: ${getScore}`;
+  score.appendChild(scoreDisplay);
 
-//map scores to UI
-const scoreDisplay = document.createElement("h3");
-scoreDisplay.innerHTML = `High score: ${getScore}`;
-score.appendChild(scoreDisplay);
-
-const initialDisplay = document.createElement("h3")
-initialDisplay.innerHTML = `Initials: ${getInitials}`
-score.appendChild(initialDisplay);
-
-
+  const initialDisplay = document.createElement("h3");
+  initialDisplay.innerHTML = `Initials: ${getInitials}`;
+  score.appendChild(initialDisplay);
+}
