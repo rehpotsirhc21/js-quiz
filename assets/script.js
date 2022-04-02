b72cf087802532328300b2b7993c5d224454437a
Christopher Langner <langnerc@icloud.com>
2022-04-01 20:29:06 -0500

    updated timer function

--- assets/script.js
+++ assets/script.js
@@ -13,9 +13,11 @@ const questionContainer = document.getElementById('question-container')
 let correct;
 
 let index;
-let highScore;
-let timeLeft = 60 * 5;
-// initial chosen question
+let highScore = 0;
+const StartTime = 1;
+let time = StartTime * 60;
+let interval;
+
 
 //functions
 
@@ -23,30 +25,22 @@ let timeLeft = 60 * 5;
 let displayTimer = document.getElementById("time-display");
 
 // fucnction for timer help from https://stackoverflow.com/questions/20618355/how-to-write-a-countdown-timer-in-javascript
-function startTimer(duration, showTimer) {
-  var timer = duration,
-    minutes,
-    seconds;
-  var timeStamp = setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
+function timer(){
+    const min = Math.floor(time/60);
+    let sec = time % 60; 
 
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    showTimer.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      timer = duration;
-      clearInterval(timeStamp);
-    }
-  }, 1000);
+    sec = sec < 10 ? '0'+ sec: sec
+    displayTimer.innerHTML = `${min}:${sec}`;
+    if(time > 0) {
+    time --}
 }
+
 
 //start game function
 function startGame() {
-  var showTimer = document.querySelector("#time");
-  startTimer(timeLeft, showTimer);
+
+ interval = setInterval(timer, 1000);
+ console.log(setInterval)
   displayTimer.classList.remove("hide");
   $(startBtnEl).addClass("hide");
   $(questionEL).removeClass("hide");
@@ -55,9 +49,11 @@ function startGame() {
   chooseQuestion(questionList[index]);
   
 }
-console.log(questionList.length)
+
 // function to choose the first question
 function chooseQuestion(question) {
+
+   verifyTime();
         
   let questionChosen = questionList[index].questionText;
   
@@ -95,7 +91,7 @@ function verifyCorrect(event) {
   selectedAnswer = event.target;
   let checkCorrect = event.target.getAttribute("data-correct");
   if (checkCorrect === "true") {
-    //   TODO: need to add logic to add to the highscore
+    
     this.classList.remove(
       "btn",
       "btn-outline-primary",
@@ -106,8 +102,10 @@ function verifyCorrect(event) {
       
     );
     this.classList.add("btn", "btn-success", "mt-3", "mr-3", "col", "col-md-4", );
+    highScore++
+    console.log(highScore)
   } else {
-    //    TODO: need to add logic to subtract time
+    
     this.classList.remove(
       "btn",
       "btn-outline-primary",
@@ -116,14 +114,30 @@ function verifyCorrect(event) {
       "col",
       "col-md-4", 
       
-
     );
     this.classList.add("btn", "btn-danger", "mt-3", "mr-3", "col", "col-md-4", "btn-large");
+    if (time >= 15) {
+    time = time - 15
+    } else {
+        time = 0
+    }
   }
   $(nextBtnEl).removeClass("hide")
   
 }
 
+
+function verifyTime() {
+    if(time <= 0) {
+        clearInterval(interval)
+        displayTimer.classList.add("hide");
+        questionEL.classList.add("hide");
+        answerEL.classList.add("hide");
+        location.reload()
+        
+        
+    }
+}
 function resetAnswers() {
     $(nextBtnEl).addClass("hide"); 
     while (answerEL.firstChild) {
@@ -132,6 +146,8 @@ function resetAnswers() {
     chooseQuestion();
     
 }
+
+
 
 function finishGame()  {
 
